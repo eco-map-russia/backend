7abb9d47667687b416df39084f2f7c9071326e27
i.shinova <qymaq@dorenue.ru>
2025-08-11 21:12:43 +0300

    Implement OpenAPI specification for user profile

--- src/main/java/com/example/eco_map/api/controller/UserController.java
+++ src/main/java/com/example/eco_map/api/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.example.eco_map.api.controller;
 
-import com.example.eco_map.security.CustomUserDetails;
+import com.example.eco_map.api.UsersApi;
+import com.example.eco_map.security.CurrentUserService;
 import com.example.eco_map.usecases.UserService;
 import com.example.eco_map.usecases.dto.UserDto;
 import com.example.eco_map.usecases.dto.UserUpdateRequestDto;
@@ -8,29 +9,41 @@ import com.example.eco_map.usecases.dto.UserUpdateResponseDto;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/me")
-public class UserController {
+@RequestMapping("/api/v1/me")
+public class UserController implements UsersApi {
     private final UserService userService;
+    private final CurrentUserService currentUserService;
 
+    @Override
     @GetMapping
-    public Mono<ResponseEntity<UserDto>> getMe(@AuthenticationPrincipal CustomUserDetails user) {
-        return userService.getCurrentUser(user.getUser().getId()).map(ResponseEntity::ok);
+    public Mono<ResponseEntity<UserDto>> getCurrentUserProfile(ServerWebExchange exchange) {
+        return currentUserService.getCurrentUser()
+                .flatMap(user -> userService.getCurrentUser(user.getId()))
+                .map(ResponseEntity::ok);
     }
 
     @PatchMapping
-    public Mono<ResponseEntity<UserUpdateResponseDto>> updateMe(@AuthenticationPrincipal CustomUserDetails user,
-                                                                @RequestBody @Valid UserUpdateRequestDto dto) {
-        return userService.updateUser(user.getUser().getId(), dto).map(ResponseEntity::ok);
+    @Override
+    public Mono<ResponseEntity<UserUpdateResponseDto>> updateCurrentUserProfile(
+            @RequestBody @Valid Mono<UserUpdateRequestDto> userUpdateRequestDto,
+            ServerWebExchange exchange) {
+        return currentUserService.getCurrentUser()
+                .zipWith(userUpdateRequestDto)
+                .flatMap(tuple -> {
+                    var user = tuple.getT1();
+                    var userDto = tuple.getT2();
+                    return userService.updateUser(user.getId(), userDto);
+                })
+                .map(ResponseEntity::ok);
     }
-
 }
